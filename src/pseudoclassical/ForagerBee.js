0dbc9c5e7f3d1d9f8566a5881a1ae3c40df2ffb3
lmt <1571min@naver.com>
2020-03-26 11:49:50 +0900

    update clasical

--- src/pseudoclassical/ForagerBee.js
+++ src/pseudoclassical/ForagerBee.js
@@ -1,7 +1,18 @@
 var Bee = require('./Bee');
 
-var ForagerBee = function () {
-
+var ForagerBee = function() {
+  Bee.apply(this, arguments);
+  this.age = 10;
+  this.job = 'find pollen';
+  this.color = this.color;
+  this.canFly = true;
+  this.treasureChest = [];
+  this.forage = function() {
+    this.treasureChest.push('treasure');
+  };
 };
+
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.constructor = ForagerBee;
 
 module.exports = ForagerBee;
--- src/pseudoclassical/HoneyMakerBee.js
+++ src/pseudoclassical/HoneyMakerBee.js
@@ -1,7 +1,30 @@
 var Bee = require('./Bee');
 
-var HoneyMakerBee = function () {
-
+var HoneyMakerBee = function() {
+  Bee.apply(this, arguments);
+  this.color = this.color;
+  this.age = 10;
+  this.job = 'make honey';
+  this.honeyPot = 0;
+  this.makeHoney = function() {
+    this.honeyPot += 1;
+  };
+  this.giveHoney = function() {
+    this.honeyPot -= 1;
+  };
 };
+
+// HoneyMakerBee.prototype.color = this.color;
+
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+//console.log(HoneyMakerBee.prototype.constructor);
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+// HoneyMakerBee.prototype.makeHoney = function() {
+//   this.honeyPot += 1;
+// };
+// HoneyMakerBee.prototype.giveHoney = function() {
+//   this.honeyPot -= 1;
+// };
 
 module.exports = HoneyMakerBee;
